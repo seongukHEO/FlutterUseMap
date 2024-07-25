2b85e89275bc9c6ac53fb1365d6db27051b544fb
seongukHEO <heoseonguk3@gmail.com>
2024-07-26 04:43:26 +0900

    feat : 로그인 화면 구현

--- lib/login/login_screen.dart
+++ lib/login/login_screen.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:lottie/lottie.dart';
+import 'package:map_memo_remember_moment/login/join_screen.dart';
 
 
 class LoginScreen extends StatefulWidget {
@@ -10,14 +12,75 @@ class LoginScreen extends StatefulWidget {
 }
 
 class _LoginScreenState extends State<LoginScreen> {
+
+  final _formKey = GlobalKey<FormState>();
+
+  TextEditingController emailController = TextEditingController();
+
+  TextEditingController pwController = TextEditingController();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      resizeToAvoidBottomInset: false,
       body: SafeArea(
         child: Column(
-          // children: [
-          //   Lottie.asset('assets/lottie_home.json')
-          // ],
+          children: [
+            Center(
+              child: Container(
+                width: 300,
+                  height: 300,
+                  child: Lottie.asset('assets/lottie_home.json')
+              ),
+            ),
+            Form(
+                child:Padding(
+                  padding: const EdgeInsets.all(16.0),
+                  child: Column(
+                    children: [
+                      TextFormField(
+                        decoration: InputDecoration(
+                          border: OutlineInputBorder(),
+                          labelText: "이메일"
+                        ),
+                        controller: emailController,
+                      ),
+                      SizedBox(height: 20,),
+                      TextFormField(
+                        decoration: InputDecoration(
+                          border: OutlineInputBorder(),
+                          labelText: "비밀번호",
+                        ),
+                        obscureText: true,
+                        controller: pwController,
+                      )
+                    ],
+                  ),
+                )
+            ),
+            Padding(
+              padding: const EdgeInsets.all(16.0),
+              child: Column(
+                children: [
+                  MaterialButton(
+                      onPressed: (){},
+                      height: 48,
+                      minWidth: double.infinity,
+                      child: Text("로그인", style: TextStyle(fontWeight: FontWeight.bold, fontSize: 16),),
+                      color: Colors.teal,
+                  ),
+                ],
+              ),
+            ),
+            TextButton(
+                onPressed: (){
+                  Navigator.push(context, MaterialPageRoute(builder: (context){
+                    return JoinScreen();
+                  }));
+                },
+                child: Text("회원가입 하러가기", style: TextStyle(color: Colors.black),)
+            ),
+          ],
         ),
       ),
     );
