800d51599e902b2bdee295060fc1b41f6409612e
seongukHEO <heoseonguk3@gmail.com>
2024-07-31 17:26:19 +0900

    feat : 지도 마커 생성 구현

--- lib/map.dart
+++ lib/map.dart
@@ -1,50 +1,81 @@
+import 'dart:async';
+import 'dart:math';
 import 'package:flutter/material.dart';
 import 'package:flutter_naver_map/flutter_naver_map.dart';
-import 'dart:async';
 
-class NaverMapApp extends StatefulWidget {
-  const NaverMapApp({super.key});
 
-  @override
-  _NaverMapAppState createState() => _NaverMapAppState();
+void main() {
+  runApp(MyApp());
 }
 
-
-class _NaverMapAppState extends State<NaverMapApp> {
-  final Completer<NaverMapController> _controller = Completer();
-
+class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text("000님의 기억지도"),
-          leading: IconButton(
-            onPressed: (){
-              Navigator.pop(context);
-            },
-            icon: Icon(Icons.arrow_back),
-          ),
-        ),
-        body: NaverMap(
-          onMapReady: (controller) {
-            _controller.complete(controller);
-          },
-          options: NaverMapViewOptions(
-            initialCameraPosition: NCameraPosition(
-              target: NLatLng(37.5665, 126.9780), // Seoul coordinates
-              zoom: 15,
-            ),
-            indoorEnable: true,
-            locationButtonEnable: false,
-            consumeSymbolTapEvents: false,
-          ),
-          onMapTapped: (point, latLng) {
-            print("위도 : ${latLng.longitude}, 경도 : ${latLng.latitude}");
-          },
-        ),
-      ),
+      home: NaverMapApp(),
     );
   }
 }
 
+class NaverMapApp extends StatefulWidget {
+  @override
+  _NaverMapAppState createState() => _NaverMapAppState();
+}
+
+class _NaverMapAppState extends State<NaverMapApp> {
+  final Completer<NaverMapController> _controller = Completer();
+  NaverMapController? _mapController;
+  final List<NMarker> _markers = [];
+
+  void _onMapTap(Point point, NLatLng latlng) async {
+    final newMarker = NMarker(
+      id: "test_${DateTime.now().millisecondsSinceEpoch}",
+      position: latlng,
+      caption: NOverlayCaption(
+        text: "위도: ${latlng.latitude}, 경도: ${latlng.longitude}",
+      ),
+    );
+
+    setState(() {
+      _markers.add(newMarker);
+    });
+
+    final controller = await _controller.future;
+    controller.addOverlay(newMarker);
+
+    print("위도 : ${latlng.latitude}, 경도 : ${latlng.longitude}");
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("기억지도"),
+        leading: IconButton(
+          onPressed: () {
+            Navigator.pop(context);
+          },
+          icon: Icon(Icons.arrow_back),
+        ),
+      ),
+      body: NaverMap(
+        onMapReady: (controller) {
+          _controller.complete(controller);
+          setState(() {
+            _mapController = controller;
+          });
+        },
+        options: NaverMapViewOptions(
+          initialCameraPosition: NCameraPosition(
+            target: NLatLng(37.5665, 126.9780), // Seoul coordinates
+            zoom: 15,
+          ),
+          indoorEnable: true,
+          locationButtonEnable: false,
+          consumeSymbolTapEvents: false,
+        ),
+        onMapTapped: _onMapTap,
+      ),
+    );
+  }
+}
